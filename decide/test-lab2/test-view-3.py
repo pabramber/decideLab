0d02b20dc41eea88668512c744053e2a1664577d
pabramber <pabramber@alum.us.es>
2022-10-13 09:29:55 +0200

    Fix

--- decide/test-lab2/test-view-3.py
+++ decide/test-lab2/test-view-3.py
@@ -21,8 +21,9 @@ class AdminTestCase(StaticLiveServerTestCase):
 
         options = webdriver.ChromeOptions()
         options.headless = False
-        options.add_argument("--disable-dev-shm-using")
         options.add_argument("--no-sandbox")
+        options.add_argument("--remote-debugging-port=9222")
+        options.add_argument("--disable-dev-shm-using")
         self.driver = webdriver.Chrome(options=options)
 
         super().setUp()            
