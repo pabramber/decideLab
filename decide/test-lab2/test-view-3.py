781ca655ec030a15b146bbd5a24741b1894a033c
pabramber <pabramber@alum.us.es>
2022-10-13 10:14:59 +0200

    Fix

--- decide/test-lab2/test-view-3.py
+++ decide/test-lab2/test-view-3.py
@@ -21,10 +21,11 @@ class AdminTestCase(StaticLiveServerTestCase):
 
         options = webdriver.ChromeOptions()
         options.headless = False
+        options.add_argument("start-maximized")
         options.add_argument("--no-sandbox")
         options.add_argument("--remote-debugging-port=9222")
         options.add_argument("--disable-dev-shm-using")
-        self.driver = webdriver.Chrome(options=options)
+        self.driver = webdriver.Chrome("/usr/bin/chromedriver", options=options)
 
         super().setUp()            
             
